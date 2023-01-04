b12444a4a4f35751e22c3a9677df20125a35d670
abdulhadi <abdulhadi01072001@gmail.com>
2023-01-04 21:38:31 +0530

    user updated

--- controllers/propertyController.js
+++ controllers/propertyController.js
@@ -28,7 +28,7 @@ const sellProperty = asyncHandler(async(req, res)=>{
     }
 
     req.body.Photos = imageArray
-    req.body.User = req.user?.id
+    User = req.user._id
 
     const property = await Property.create(req.body)
     res.status(200).json({
